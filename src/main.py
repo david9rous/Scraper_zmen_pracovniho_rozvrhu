fc10fe8392832c66ca914033c154853b21649f4c
david9rous <david9rous@gmail.com>
2023-09-27 08:35:40 +0200

    added some initial code

---
+++ src/main.py
@@ -0,0 +1,15 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+webpage = requests.get("https://zsrovniny.bakalari.cz/next/zmeny.aspx")
+if webpage.status_code == 200:
+    print("Status: OK")
+else:
+    print("Neco se pokazilo.")
+table = BeautifulSoup(webpage.content, 'html.parser')
+schedule = table.find('table', {'class': 'datagrid'})
+cells = []
+rows = schedule.find_all('<tr>')
+for row in rows:
+    print(row)
