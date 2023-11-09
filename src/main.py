aa3b52d8363e87c3dd339aa69bf338bb1aafcb70
david9rous <david9rous@gmail.com>
2023-11-09 07:34:16 +0100

    0001

--- src/main.py
+++ src/main.py
@@ -2,13 +2,16 @@ import requests
 from bs4 import BeautifulSoup
 
 
-webpage = requests.get("https://zsrovniny.bakalari.cz/next/zmeny.aspx")
-if webpage.status_code == 200:
+url = requests.get("https://zsrovniny.bakalari.cz/next/zmeny.aspx")
+if url.status_code == 200:
     print("Status: OK")
 else:
     print("Neco se pokazilo.")
-table = BeautifulSoup(webpage.content, 'html.parser')
-schedule = table.find_all('table', {'class': 'datagrid'})
-text = 'Rouš'
-for i in schedule:
-    element = i.find(text)
+soup = BeautifulSoup(url.content, 'html.parser')
+tables = soup.find_all('table', {'class': 'datagrid'})
+text = 'ABD'
+for table in tables:
+    for row in table.find_all('tr'):
+        for cell in row.find_all('td'):
+            if text in cell.text():
+                print('Nalezeno')
