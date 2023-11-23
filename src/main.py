bf2b0605e443706d4287c92e23224effeda71f95
David Rouš <137796501+david9rous@users.noreply.github.com>
2023-11-23 11:43:27 +0000

    2311231243

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 """Scrapes the school website for changes in the schedule and prints them in the terminal."""
 
 
+from datetime import date
 import requests
 from bs4 import BeautifulSoup
 
@@ -17,7 +18,14 @@ for table in tables:
     for row in table.find_all('tr'):
         for cell in row.find_all('td'):
             if SEARCHED_TEXT in cell.text:
-                print(row.prettify())
-                with open('stored_information.csv', 'a') as file:
-                    file.write(row)
-                    
+                sub_info = row.get_text()
+sub_info = sub_info.replace('\n', ' ')
+print(sub_info)
+today_date = str(date.today())
+with open('stored_information.csv', 'a') as file:
+    file.write('Datum: \n')
+    file.write(today_date)
+    file.write('\n')
+    file.write('Zmeny: \n')
+    file.write(sub_info)
+    file.write('\n\n')
