7261fc70a25f7f2cadb5d8ff8fd20c3cb309e201
David Rouš <137796501+david9rous@users.noreply.github.com>
2023-11-24 07:30:40 +0000

    2311240830

--- src/main.py
+++ src/main.py
@@ -23,14 +23,14 @@ SUB_INFO = ' '.join(sub_info.split())
 TODAY_DATE = str(date.today())
 with open('data/stored_sub_info.txt', 'a+', encoding="utf-8") as file:
     file.seek(0)
-    contents = file.read()
+    contents = file.read().split('Datum:')
     print(contents)
     for ele in contents:
         print(ele)
-        # if contents.__contains__(TODAY_DATE):
-        #     file.write('Datum: \n')
-        #     file.write(TODAY_DATE)
-        #     file.write('\n')
-        #     file.write('Zmeny: \n')
-        #     file.write(SUB_INFO)
-        #     file.write('\n\n')
+        if TODAY_DATE not in contents:
+            file.write('Datum: \n')
+            file.write(TODAY_DATE)
+            file.write('\n')
+            file.write('Zmeny: \n')
+            file.write(SUB_INFO)
+            file.write('\n\n')
