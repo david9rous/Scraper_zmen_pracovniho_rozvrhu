26e7c0d23c040db2a774a2a9c7ce20dea65b6de9
David Rouš <david9rous@gmail.com>
2023-11-28 15:18:22 +0100

    2311281518

--- src/testing.py
+++ src/testing.py
@@ -20,12 +20,14 @@ for row in table.find_all('tr'):
     for cell in row.find_all('td'):
         if SEARCHED_TEXT in cell.text:
             sub_info = row
+sub_info_list = []
+for data in sub_info.find_all('td'):
+    sub_info_list.append(data.get_text().split())
+print(sub_info_list)
 
-# sub_info_list = []
 # for line in sub_info:
 #     sub_info_list.append(line)
 # sub_info = ' '.join(sub_info.split())
 # sub_info = re.split(f'({SEARCHED_TEXT})', sub_info)
 # if sub_info[0] == '':
     # sub_info = sub_info[1:]
-print(sub_info)
